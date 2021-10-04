dc284f4849e836517cf10e96b86d9dc4a4973231
Jon Oh <jonathanoh552@gmail.com>
2021-10-04 10:45:15 -0700

    diceRoll 1.0.2, finished bot!

--- source.py
+++ source.py
@@ -4,7 +4,6 @@ import random
 client = discord.Client()
 
 @client.event
-
 async def on_message(message):
     if message.author == client.user:
         return
@@ -13,4 +12,4 @@ async def on_message(message):
         await channel.send(str(random.randrange(1,7)))
         print(str(random.randrange(1,7)))
 
-client.run("ODkzODA0OTA2NDEzMTgyOTk3.YVgyrw.9zaKWinIWUuDmXdj8qB98V_5BMk")
+client.run('ODkzODA0OTA2NDEzMTgyOTk3.YVgyrw.X7Jp1HYHXTIYLMcVVNxiBfGPXU0')
